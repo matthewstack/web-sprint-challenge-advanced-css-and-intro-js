331325c42f7a1b6543cd42e112e15b63efecefcc
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-07-16 13:01:55 -0400

    Task 5

--- index.js
+++ index.js
@@ -209,11 +209,11 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-console.log(artists[0].name);
+console.log('task1a', artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-console.log(artists[2].name);
+console.log('task1b', artists[2].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -221,7 +221,7 @@ console.log(artists[2].name);
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
 artists.splice(9,1, {'name':'Vincent Van Gogh'})
-console.log(artists[9].name)
+console.log('task 2', artists[9].name)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -234,6 +234,7 @@ Example, if getArtistByIndex is invoked with the artists array and the number 0,
 function getArtistByIndex(array, num1) {
   return `the artist at index ${num1} is ${array[num1].name}`
 }
+console.log('task3', getArtistByIndex(artists, 0))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use get20s to do the following: 
@@ -244,9 +245,17 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
 }
+//   const twentiethCentury = [];
+//   for (let i = 0; i < array.length; i++)
+//     if (math.floor((array[i].years.split(" - ")/100)*100 === 1900)){
+//       twentiethCentury.push(array[i].name)
+//     }
+//     return twentiethCentury;
+//     // if (array[i].years.split(" - ") => 1900 && array[i].years.split(" - ") <= 2000)
+// }
+// console.log(get20s(artists))
 
 
 
@@ -259,10 +268,11 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, num1) {
+  array.splice(num1, 1)
+  return array.length;
 }
-
+console.log('task5', removeArtist(artists, 0))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
